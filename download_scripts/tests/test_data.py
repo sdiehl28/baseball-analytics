0c03e85aa37791820b2c7fb5e7e929de9772327c
Stephen Diehl <sdiehl@fastimap.com>
2019-12-28 19:34:41 -0800

    part 1 of verify retro and lahman fielding data is consistent

--- download_scripts/tests/test_data.py
+++ download_scripts/tests/test_data.py
@@ -148,7 +148,7 @@ def test_game_pkey(game):
 
 
 def test_lahman_retro_batting_data(data_dir, batting):
-    """Compare Aggregated Lahman data to Aggregated Retrosheet data
+    """Compare Aggregated Lahman batting data to Aggregated Retrosheet batting data
 
     The scripts must have worked correctly if the 16 batting fields
     in common have almost exactly the same sum between 1974 and 2019
@@ -167,6 +167,9 @@ def test_lahman_retro_batting_data(data_dir, batting):
     b_cols = set(batting.columns) & set(lahman_batting.columns)
     b_cols -= {'player_id'}
 
+    # there are 16 columns in common
+    assert len(b_cols) == 16
+
     l_batting = l_batting[b_cols]
     r_batting = r_batting[b_cols]
 
@@ -179,7 +182,7 @@ def test_lahman_retro_batting_data(data_dir, batting):
 
 
 def test_lahman_retro_pitching_data(data_dir, pitching):
-    """Compare Aggregated Lahman data to Aggregated Retrosheet data
+    """Compare Aggregated Lahman pitching data to Aggregated Retrosheet pitching data
 
     The scripts must have worked correctly if the 19 pitching fields
     in common have almost exactly the same sum between 1974 and 2019
@@ -198,6 +201,9 @@ def test_lahman_retro_pitching_data(data_dir, pitching):
     p_cols = set(l_pitching.columns) & set(r_pitching.columns)
     p_cols -= {'player_id'}
 
+    # there are 19 columns in common
+    assert len(p_cols) == 19
+
     l_pitching = l_pitching[p_cols]
     r_pitching = r_pitching[p_cols]
 
@@ -206,3 +212,56 @@ def test_lahman_retro_pitching_data(data_dir, pitching):
 
     # verify all values between 1974 and 2019 are within plus/minus 0.06% of each other
     assert (np.abs(1.0 - (l_sums / r_sums)) < .0006).all()
+
+
+def test_lahman_retro_fielding_data(data_dir, fielding):
+    """Compare Aggregated Lahman fielding per position data to
+    Aggregated Retrosheet fielding per position data
+
+    The scripts must have worked correctly if the 7 fielding fields
+    in common have almost exactly the same sum between 1974 and 2019
+    for every position.
+    """
+    filename = data_dir / 'lahman' / 'wrangled' / 'fielding.csv'
+    lahman_fielding = dh.from_csv_with_types(filename)
+
+    # Retrosheet data has no missing games since 1974
+    filt = (lahman_fielding['year_id'] >= 1974) & (lahman_fielding['year_id'] <= 2019)
+    l_fielding = lahman_fielding[filt]
+
+    filt = (fielding['game_id'].str[3:7] >= '1974') & (fielding['game_id'].str[3:7] <= '2019')
+    r_fielding = fielding.loc[filt]
+
+    r_fielding = r_fielding.rename(columns={'out': 'inn_outs'})
+
+    # find the common columns
+    f_cols = (set(l_fielding.columns) & set(r_fielding.columns)) - set(['player_id', 'pos'])
+    f_cols = list(f_cols)
+
+    l_sums = l_fielding.groupby('pos')[f_cols].aggregate('sum')
+    l_sums.sort_index(inplace=True)
+
+    # change position values to be lower case
+    idxs = {idx: idx.lower() for idx in l_sums.index}
+    l_sums.rename(index=idxs, inplace=True)
+
+    # there are 7 fielding attributes and 7 fielding positions in Lahman
+    assert l_sums.shape == (7, 7)
+
+    r_sums = r_fielding.groupby('pos')[f_cols].aggregate('sum').astype('int')
+
+    # Lahman uses OF for sum of LF, CF, RF
+    r_sums.loc['of'] = r_sums.loc['lf'] + r_sums.loc['cf'] + r_sums.loc['rf']
+    r_sums = r_sums.drop(['lf', 'cf', 'rf'])
+    r_sums.sort_index(inplace=True)
+
+    # there are 7 fielding attributes and 7 fielding positions in Retrosheet
+    assert r_sums.shape == (7, 7)
+
+    # the indexes and columns should now be the same
+    assert l_sums.index.equals(r_sums.index)
+    assert l_sums.columns.equals(r_sums.columns)
+
+    #
+    # # verify all values between 1974 and 2019 are within plus/minus 0.06% of each other
+    # assert (np.abs(1.0 - (l_sums / r_sums)) < .0006).all()
