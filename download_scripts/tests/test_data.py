f064ea4cdd6edc40f927e8878badd7fe6257bc24
Stephen Diehl <sdiehl@fastimap.com>
2020-01-10 17:41:38 -0800

    rewrite consistency tests involving team_game

--- download_scripts/tests/test_data.py
+++ download_scripts/tests/test_data.py
@@ -70,7 +70,7 @@ def test_lahman_fielding_pkey(data_dir):
 
     # check for duplicate IDs
     fielding = dh.from_csv_with_types(filename)
-    assert dh.is_unique(fielding, ['player_id', 'year_id', 'stint', 'pos'])
+    assert dh.is_unique(fielding, ['player_id', 'year', 'stint', 'pos'])
 
 
 def test_lahman_batting_pkey(data_dir):
@@ -78,7 +78,7 @@ def test_lahman_batting_pkey(data_dir):
 
     # check for duplicate IDs
     batting = dh.from_csv_with_types(filename)
-    assert dh.is_unique(batting, ['player_id', 'year_id', 'stint'])
+    assert dh.is_unique(batting, ['player_id', 'year', 'stint'])
 
 
 def test_lahman_pitching_pkey(data_dir):
@@ -86,7 +86,7 @@ def test_lahman_pitching_pkey(data_dir):
 
     # check for duplicate IDs
     pitching = dh.from_csv_with_types(filename)
-    assert dh.is_unique(pitching, ['player_id', 'year_id', 'stint'])
+    assert dh.is_unique(pitching, ['player_id', 'year', 'stint'])
 
 
 def test_lahman_salaries_pkey(data_dir):
@@ -94,7 +94,7 @@ def test_lahman_salaries_pkey(data_dir):
 
     # check for duplicate IDs
     salaries = dh.from_csv_with_types(filename)
-    assert dh.is_unique(salaries, ['player_id', 'year_id', 'team_id'])
+    assert dh.is_unique(salaries, ['player_id', 'year', 'team_id'])
 
 
 def test_lahman_teams_pkey(data_dir):
@@ -102,8 +102,8 @@ def test_lahman_teams_pkey(data_dir):
 
     # check for duplicate IDs
     teams = dh.from_csv_with_types(filename)
-    assert dh.is_unique(teams, ['team_id', 'year_id'])  # lahman team_id
-    assert dh.is_unique(teams, ['team_id_retro', 'year_id'])  # retrosheet team_id
+    assert dh.is_unique(teams, ['team_id', 'year'])  # lahman team_id
+    assert dh.is_unique(teams, ['team_id_retro', 'year'])  # retrosheet team_id
 
 
 def test_lahman_parks_pkey(data_dir):
@@ -185,24 +185,18 @@ def test_game_pkey(game):
 
 def test_lahman_retro_batting_data(data_dir, batting):
     """Compare Aggregated Lahman batting data to Aggregated Retrosheet batting data
-
-    The scripts must have worked correctly if the 17 batting fields
-    in common have almost exactly the same sum between 1974 and 2019
     """
     filename = data_dir / 'lahman' / 'wrangled' / 'batting.csv'
     lahman_batting = dh.from_csv_with_types(filename)
 
     # Retrosheet data has no missing games since 1974
-    filt = (batting['game_id'].str[3:7] >= '1974') & (batting['game_id'].str[3:7] <= '2019')
-    r_batting = batting.loc[filt]
+    r_batting = batting.query('1974 <= game_start_dt.dt.year <= 2019')
 
     # this could differ from 1974 and 2019 if not all those years were downloaded
-    retro_min_year = r_batting['game_id'].str[3:7].astype('int').min()
-    retro_max_year = r_batting['game_id'].str[3:7].astype('int').max()
+    min_year = r_batting['game_start_dt'].agg('min').year
+    max_year = r_batting['game_start_dt'].agg('max').year
 
-    filt = (lahman_batting['year_id'] >= retro_min_year) & \
-           (lahman_batting['year_id'] <= retro_max_year)
-    l_batting = lahman_batting[filt]
+    l_batting = lahman_batting.query('@min_year <= year <= @max_year')
 
     # columns in common -- these are the columns to compare
     b_cols = set(batting.columns) & set(lahman_batting.columns)
@@ -235,16 +229,13 @@ def test_lahman_retro_pitching_data(data_dir, pitching):
     lahman_pitching = dh.from_csv_with_types(filename)
 
     # Retrosheet data has no missing games since 1974
-    filt = (pitching['game_id'].str[3:7] >= '1974') & (pitching['game_id'].str[3:7] <= '2019')
-    r_pitching = pitching.loc[filt]
+    r_pitching = pitching.query('1974 <= game_start_dt.dt.year <= 2019')
 
     # this could differ from 1974 and 2019 if not all those years were downloaded
-    retro_min_year = r_pitching['game_id'].str[3:7].astype('int').min()
-    retro_max_year = r_pitching['game_id'].str[3:7].astype('int').max()
+    min_year = r_pitching['game_start_dt'].agg('min').year
+    max_year = r_pitching['game_start_dt'].agg('max').year
 
-    filt = (lahman_pitching['year_id'] >= retro_min_year) & \
-           (lahman_pitching['year_id'] <= retro_max_year)
-    l_pitching = lahman_pitching[filt]
+    l_pitching = lahman_pitching.query('@min_year <= year <= @max_year')
 
     # columns in common -- these are the columns to compare
     p_cols = set(l_pitching.columns) & set(r_pitching.columns)
@@ -278,17 +269,13 @@ def test_lahman_retro_fielding_data(data_dir, fielding):
     lahman_fielding = dh.from_csv_with_types(filename)
 
     # Retrosheet data has no missing games since 1974
-    filt = (fielding['game_id'].str[3:7] >= '1974') & (fielding['game_id'].str[3:7] <= '2019')
-    r_fielding = fielding.loc[filt]
-    r_fielding = r_fielding.rename(columns={'out': 'inn_outs'})
+    r_fielding = fielding.query('1974 <= game_start_dt.dt.year <= 2019')
 
     # this could differ from 1974 and 2019 if not all those years were downloaded
-    retro_min_year = r_fielding['game_id'].str[3:7].astype('int').min()
-    retro_max_year = r_fielding['game_id'].str[3:7].astype('int').max()
+    min_year = r_fielding['game_start_dt'].agg('min').year
+    max_year = r_fielding['game_start_dt'].agg('max').year
 
-    filt = (lahman_fielding['year_id'] >= retro_min_year) & \
-           (lahman_fielding['year_id'] <= retro_max_year)
-    l_fielding = lahman_fielding[filt]
+    l_fielding = lahman_fielding.query('@min_year <= year <= @max_year')
 
     # find the common columns
     f_cols = (set(l_fielding.columns) & set(r_fielding.columns)) - {'player_id', 'pos', 'team_id'}
@@ -333,21 +320,19 @@ def test_batting_team_game_data(batting, team_game):
     is the same a team_game by (game_id, team_id)
 
     This shows that the two Retrosheet parsers are consistent with one another."""
-    # team_game batting columns
-    pkey = ['game_id', 'team_id']
+    exclude = ['game_id', 'team_id', 'player_id', 'game_start_dt']
+    cols = set(batting.columns) & set(team_game.columns) - set(exclude)
+    cols = list(cols)
 
-    # batting columns to compare
-    compare_cols = set(batting.columns) & set(team_game.columns) - set(pkey)
-    compare_cols = list(compare_cols)
+    assert len(cols) == 17
 
-    assert len(compare_cols) == 17
+    b = batting[['game_id', 'team_id'] + cols].groupby(['game_id', 'team_id']).agg('sum')
+    b = b.reset_index().sort_index()
 
-    # sum all batting columns by the pkey of team_game, sort and reindex
-    a = batting.groupby(['game_id', 'team_id'], as_index=False)[compare_cols].sum()
-    a = a.sort_values(['game_id', 'team_id']).reset_index(drop=True)
-    b = team_game[pkey + compare_cols].sort_values(['game_id', 'team_id']).reset_index(drop=True)
+    tg = team_game[['game_id', 'team_id'] + cols].sort_values(
+        ['game_id', 'team_id']).reset_index(drop=True)
 
-    assert a.equals(b)
+    assert b.equals(tg)
 
 
 def test_pitching_team_game_data(batting, pitching, team_game):
@@ -355,22 +340,15 @@ def test_pitching_team_game_data(batting, pitching, team_game):
     is the same as team_game Pitching by (game_id, opponent_team_id)
 
     This shows that the two Retrosheet parsers are consistent with one another."""
-    # team_game batting columns
-    pkey = ['game_id', 'team_id']
+    cols = ['wp', 'bk', 'er']
 
-    # columns to compare
-    compare_cols = set(pitching.columns) & set(batting.columns) & set(team_game.columns) - set(pkey)
-    compare_cols = list(compare_cols)
+    p = pitching[['game_id', 'team_id'] + cols].groupby(['game_id', 'team_id']).agg('sum')
+    p = p.reset_index().sort_index()
 
-    assert len(compare_cols) == 14
+    tg = team_game[['game_id', 'team_id'] + cols].sort_values(
+        ['game_id', 'team_id']).reset_index(drop=True)
 
-    a = pitching.groupby(['game_id', 'team_id'], as_index=False)[compare_cols].sum()
-    a = a.sort_values(['game_id', 'team_id']).reset_index(drop=True)
-
-    b = team_game[['game_id', 'opponent_team_id'] + compare_cols].sort_values(
-        ['game_id', 'opponent_team_id']).reset_index(drop=True)
-
-    assert a[compare_cols].equals(b[compare_cols])
+    assert p.equals(tg)
 
 
 def test_fielding_team_game_data(fielding, team_game):
@@ -378,28 +356,19 @@ def test_fielding_team_game_data(fielding, team_game):
     is the same a team_game by (game_id, team_id)
 
     This shows that the two Retrosheet parsers are consistent with one another."""
-    pkey = ['game_id', 'team_id']
+    cols = ['a', 'e', 'po', 'pb']
 
-    # columns to compare
-    compare_cols = set(fielding.columns) & set(team_game.columns) - set(pkey)
+    f = fielding[['game_id', 'team_id'] + cols].groupby(['game_id', 'team_id']).agg('sum')
+    f = f.reset_index().sort_index()
 
-    # in fielding dp and tp are for those players involved, so this over counts
-    # the total dp and tp in a game
-    # there are more types of xi than just those by defensive players
-    compare_cols -= {'dp', 'tp', 'xi'}
-    compare_cols = list(compare_cols)
+    tg = team_game[['game_id', 'team_id'] + cols].sort_values(
+        ['game_id', 'team_id']).reset_index(drop=True)
 
-    assert len(compare_cols) == 4
-
-    a = fielding.groupby(['game_id', 'team_id'], as_index=False)[compare_cols].sum()
-    a = a.sort_values(['game_id', 'team_id']).reset_index(drop=True)
-    b = team_game[pkey + compare_cols].sort_values(['game_id', 'team_id']).reset_index(drop=True)
-
-    assert a.equals(b)
+    assert f.equals(tg)
 
 
 def test_batting_lahman_game_data(data_dir, batting):
-    """Verify Retrosheet batting aggregated by (year_id, team_id_lahman)
+    """Verify Retrosheet batting aggregated by (year, team_id_lahman)
     is the same as lahman_teams
 
     This shows that Retrosheet batting and Lahman Teams are consistent with each other."""
@@ -409,20 +378,20 @@ def test_batting_lahman_game_data(data_dir, batting):
 
     # Add year and only select between 1974 and 2019
     retro_batting = batting.copy()
-    retro_batting['year_id'] = retro_batting['game_id'].str[3:7].astype('int')
-    year_filt = (retro_batting['year_id'] >= 1974) & (retro_batting['year_id'] <= 2019)
+    retro_batting['year'] = retro_batting['game_id'].str[3:7].astype('int')
+    year_filt = (retro_batting['year'] >= 1974) & (retro_batting['year'] <= 2019)
     retro_batting = retro_batting[year_filt].copy()
 
     # Add team_id_lahman
-    retro_batting = pd.merge(retro_batting, lahman_teams[['team_id', 'year_id', 'team_id_retro']],
-                             left_on=['year_id', 'team_id'],
-                             right_on=['year_id', 'team_id_retro'],
+    retro_batting = pd.merge(retro_batting, lahman_teams[['team_id', 'year', 'team_id_retro']],
+                             left_on=['year', 'team_id'],
+                             right_on=['year', 'team_id_retro'],
                              how='inner', suffixes=['_retrosheet', '_lahman'])
 
     # team_id_retro is now the same as team_id_retrosheet
     retro_batting.drop('team_id_retro', axis=1, inplace=True)
 
-    pkey = ['year_id', 'team_id']
+    pkey = ['year', 'team_id']
     compare_cols = set(lahman_teams.columns) & set(retro_batting.columns) - set(pkey)
     compare_cols -= {'g'}  # cannot sum g by player per team to get g per team
     compare_cols -= {'sb', 'cs'}  # these stats are close, but don't tie out as well as others
@@ -430,11 +399,11 @@ def test_batting_lahman_game_data(data_dir, batting):
 
     assert len(compare_cols) == 10
 
-    retro_batting_sums = retro_batting.groupby(['year_id', 'team_id_lahman'])[compare_cols].sum().astype('int')
+    retro_batting_sums = retro_batting.groupby(['year', 'team_id_lahman'])[compare_cols].sum().astype('int')
     retro_batting_sums.sort_index(inplace=True)
 
-    year_min, year_max = retro_batting['year_id'].aggregate(['min', 'max'])
-    year_filt = (lahman_teams['year_id'] >= year_min) & (lahman_teams['year_id'] <= year_max)
+    year_min, year_max = retro_batting['year'].aggregate(['min', 'max'])
+    year_filt = (lahman_teams['year'] >= year_min) & (lahman_teams['year'] <= year_max)
     l_teams = lahman_teams.loc[year_filt, pkey + compare_cols]
     l_teams = l_teams.set_index(pkey).sort_index()
 
@@ -482,18 +451,19 @@ def test_sky_condition_values(game):
     valid_values = ['sunny', 'cloudy', 'overcast', 'night', 'dome']
     assert game['sky_condition'].dropna().isin(valid_values).all()
 
+
 def test_game_length_values(game):
     outs = game['game_length_outs']
     inns = game['game_length_innings']
 
     # this is defined by the rules of baseball
-    assert ((5 * inns  <= outs) & (outs <= 6 * inns)).all()
+    assert ((5 * inns <= outs) & (outs <= 6 * inns)).all()
+
 
 def test_game_length_minute_values(game):
     outs = game['game_length_outs']
     mins = game['game_length_minutes']
-    mins_per_out = mins/outs
+    mins_per_out = mins / outs
 
     # these bounds should be wide enough to encompass any future game
     assert ((mins_per_out.min() > 1) & (mins_per_out.max() < 6)).all()
-
